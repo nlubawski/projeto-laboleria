b5619ed7b4a3cfdcdf64c636382a73257ba0ebab
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-08-24 20:07:33 -0300

    create getOrderRepository

--- src/repositories/order.repository.js
+++ src/repositories/order.repository.js
@@ -8,3 +8,24 @@ export async function saveOrderRepository(clientId, cakeId, quantity, totalPrice
   }
 }
 
+export function getOrderRepository(date) {
+    const query = `
+      SELECT
+        orders.id, orders.clientId, orders.cakeId, orders.quantity, TO_CHAR(orders.createdAt, 'YYYY-MM-DD HH24:MI') AS time,
+        orders.totalPrice, clients.name, clients.address, clients.phone, cakes.name,
+        cakes.price, cakes.image, cakes.description
+      FROM orders
+      JOIN clients ON orders.clientId = clients.id
+      JOIN cakes ON orders.cakeId = cakes.id
+      WHERE 1=1`;
+  
+    const queryParams = [];
+    let queryComplement = '';
+
+    if (date) {
+        queryParams.push(date);
+        queryComplement = ` AND TO_CHAR(orders.createdAt, 'YYYY-MM-DD') = $${queryParams.length}`;
+    }
+
+    return db.query(query + queryComplement, queryParams);
+}
